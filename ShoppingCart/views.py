733898c1b9ee69c4294c8356205c7be631b5c1b3
AdamTutor <atutor@basecampcodingacademy.org>
2017-03-23 10:48:47 -0500

    linked all cs files to templates

--- ShoppingCart/views.py
+++ ShoppingCart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "ShoppingCart/homepage.html")
+
+
+def store(request):
+    return render(request, "ShoppingCart/store.html")
