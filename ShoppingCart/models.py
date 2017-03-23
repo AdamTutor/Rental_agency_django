6d7d2235cf7b3423d2637af0744f4d62f858d031
AdamTutor <atutor@basecampcodingacademy.org>
2017-03-23 10:55:21 -0500

    added model for Item and Brand

--- ShoppingCart/models.py
+++ ShoppingCart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=30)
+    quantity = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+
+
+class Brand(models.Model):
+    Brand = models.CharField(max_length=30)
